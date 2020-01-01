ac5a0b208623faa57f0a25ce13a8555cdb39616d
skeri123 <dugalice@gmail.com>
2020-01-01 16:35:57 +0100

    php mailer

--- php/email.php
+++ php/email.php
@@ -1,12 +1,37 @@
 <?php
-	$email = $_POST['email'];
 	$name =  $_POST['name'];
 	$surname =  $_POST['surname'];
 	$phone =  $_POST['phone'];
-	$message =  $_POST['message'];
-	$subject = "Resume email";
 
-	mail($email,$subject,$message);
+
+    $mailto = $_POST['email'];
+    $mailSub = "Adem Mail";
+    $mailMsg = $_POST['message'];
+   require '../PHPMailer-master/PHPMailerAutoload.php';
+   $mail = new PHPMailer();
+   $mail ->IsSmtp();
+   $mail ->SMTPDebug = 0;
+   $mail ->SMTPAuth = true;
+   $mail ->SMTPSecure = 'ssl';
+   $mail ->Host = "smtp.gmail.com";
+   $mail ->Port = 465; // or 587
+   $mail ->IsHTML(true);
+   $mail ->Username = "DevIUS.ius@gmail.com";
+   $mail ->Password = "DevIUS123456789%";
+   $mail ->SetFrom("DevIUS.ius@gmail.com");
+   $mail ->Subject = $mailSub;
+   $mail ->Body = $mailMsg;
+   $mail ->AddAddress($mailto);
+   
+   if(!$mail->Send())
+   {
+       echo "Mail Not Sent";
+   }
+   else
+   {
+       echo "Mail Sent";
+   }
+
 
 
 ?>
