5131596cb5343944ca99bbdd9ea187c44c7348e7
skeri123 <dugalice@gmail.com>
2020-01-11 12:38:43 +0100

    php

--- php/email.php
+++ php/email.php
@@ -1,37 +1,36 @@
 <?php
+   require '../PHPMailer-master/PHPMailerAutoload.php';
 	$name =  $_POST['name'];
 	$surname =  $_POST['surname'];
 	$phone =  $_POST['phone'];
+  $email = $_POST['email'];
+  $mailSub = "Question from resume";
+  $mailMsg = $_POST['message'];
 
-
-    $mailto = $_POST['email'];
-    $mailSub = "Adem Mail";
-    $mailMsg = $_POST['message'];
-   require '../PHPMailer-master/PHPMailerAutoload.php';
-   $mail = new PHPMailer();
-   $mail ->IsSmtp();
-   $mail ->SMTPDebug = 0;
-   $mail ->SMTPAuth = true;
-   $mail ->SMTPSecure = 'ssl';
-   $mail ->Host = "smtp.gmail.com";
-   $mail ->Port = 465; // or 587
-   $mail ->IsHTML(true);
-   $mail ->Username = "DevIUS.ius@gmail.com";
-   $mail ->Password = "DevIUS123456789%";
-   $mail ->SetFrom("DevIUS.ius@gmail.com");
-   $mail ->Subject = $mailSub;
-   $mail ->Body = $mailMsg;
-   $mail ->AddAddress($mailto);
-   
-   if(!$mail->Send())
-   {
-       echo "Mail Not Sent";
-   }
-   else
-   {
-       echo "Mail Sent";
-   }
-
-
+    $mail = new PHPMailer();
+    $mail ->IsSmtp();
+     $mail ->SMTPDebug = 0;
+     $mail ->SMTPAuth = true;
+     $mail ->SMTPSecure = 'ssl';
+     $mail ->Host = "smtp.gmail.com";
+     $mail ->Port = 465; // or 587
+     $mail ->IsHTML(true);
+     $mail ->Username = "DevIUS.ius@gmail.com";
+     $mail ->Password = "DevIUS123456789%";
+     $mail ->SetFrom("DevIUS.ius@gmail.com");
+     $mail ->Subject = $mailSub;
+     $mail ->Body = $name." ".$surname."<br>".$phone."<br>".$email."<br><br>".$mailMsg;
+     $mail ->AddAddress("dugalice@gmail.com");
+     
+   if (filter_var($email, FILTER_VALIDATE_EMAIL) && is_numeric($phone)) {
+     $mail->Send();
+    header('Location: ../index.php');
+  }else if(is_numeric($phone)){
+    echo '<script type="text/javascript"> alert("$email is not a valid email address") </script> ';
+  }else if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo '<script type="text/javascript"> alert("$phone is not a valid number") </script> ';
+  }else{
+    echo '<script type="text/javascript"> alert("$email and $phone are not a valid") </script> ';
+  }
 
 ?>
